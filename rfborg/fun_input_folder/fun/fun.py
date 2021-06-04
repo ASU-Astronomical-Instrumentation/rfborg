ae6abac5672fdb97066399f093d536d9bccad91a
cjatkin <64757534+cjatkin@users.noreply.github.com>
2021-06-03 17:24:46 -0700

    Add files via upload

---
+++ rfborg/fun_input_folder/fun/fun.py
@@ -0,0 +1,9 @@
+##################################################################################
+#funHello.py
+#Contains a function that prints "Hello! This is fun!" when called upon
+##################################################################################
+
+def funHello():
+    print("Hello! this is fun")
+def funnerHello(a):
+    print("Wow {} is cool!").format(a))
